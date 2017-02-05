044fb58163bae606e9fe837a0509e3a92e983bea
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2017-02-05 02:21:08 +0000

    initial commit

---
+++ src/main/java/com/luiz/wines/WineExampleApplication.java
@@ -0,0 +1,12 @@
+package com.luiz.wines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WineExampleApplication.class, args);
+	}
+}
